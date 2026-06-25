d26d3146942aecac76f8fcec461f6fe21e0d54f6
u319723 <balogh.tamas@nav.gov.hu>
2026-06-25 11:34:35 +0200

    - first commit

--- js/gallery.js
+++ js/gallery.js
@@ -10,3 +10,26 @@ function unDo() {
     imageDiv.style.backgroundImage = "url('')";
     imageDiv.innerHTML = "Hover over an image below to display here.";
 }
+
+function onLoad() {
+
+    const images = document.querySelectorAll("img.preview");
+
+    console.log("images: ", images);
+
+    for (let i = 0; i < images.length; i++) {
+        images[i].setAttribute("tabIndex", "0");
+    }
+}
+
+function onFocus(previewPic) {
+    var imageDiv = document.getElementById("image");
+    imageDiv.style.backgroundImage = "url('" + previewPic.src + "')";
+    imageDiv.innerHTML = previewPic.alt;
+}
+
+function onBlur() {
+    var imageDiv = document.getElementById("image");
+    imageDiv.style.backgroundImage = "url('')";
+    imageDiv.innerHTML = "Hover over an image below to display here.";
+}
